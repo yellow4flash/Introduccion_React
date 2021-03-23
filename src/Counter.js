ce3e7c37858994d192353193237817afe26aad17
yellow4flash <48302680+yellow4flash@users.noreply.github.com>
2021-03-23 09:18:22 -0500

    comentario de useState(0)

--- src/Counter.js
+++ src/Counter.js
@@ -2,6 +2,10 @@ import React, { Fragment, useState } from "react";
 
 const counter = () => {
   // Nomenclatura de array
+  /* useState() es una función que crea internamente una variable 
+  donde podremos almacenar el estado de nuestro componente. 
+  Acepta un valor inicial para esa variable y devuelve un array 
+  con dos elementos, el valor de la variable y la función para modificarla. */
   const [count, setCount] = useState(0);
   const handleClick = (increment) => {
     if (increment) {
